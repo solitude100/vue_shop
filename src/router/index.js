485def03cfb595c0841cdcec2e10946f449f15e3
xiexuanhui <xiexuanhui@123.com>
2020-07-14 23:10:28 +0800

    修改参数功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ const Users = () => import('../components/user/Users')
 const Rights = () => import('../components/power/Rights')
 const Roles = () => import('../components/power/Roles')
 const Cate = () => import('../components/goods/Cate')
+const Params = () => import('../components/goods/Params')
 
 const routes = [
   {
@@ -44,6 +45,10 @@ const routes = [
       {
         path: '/categories',
         component: Cate
+      },
+      {
+        path: '/params',
+        component: Params
       }
     ]
   }
